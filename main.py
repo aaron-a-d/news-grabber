1344b594c0cce71d5fc22138fc2e0a88c9d808e9
Aaron Aben Danan <aarondanan@gmail.com>
2024-06-21 12:10:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -152,6 +152,7 @@ if OPENAI_KEY and SCRAPINGBEE_KEY:
         html_article = get_article_details_kan(html_text)
         article_json = ask_gpt(prompts['extract_content'].format(text=html_article), json=True)
         metadata_properties = extract_meta_properties(html_text)
+        print("metadata_properties", metadata_properties)
         og_title = ask_gpt(prompts["translate"].format(text=metadata_properties['og:title']), json=True)
         metadata_properties['og:title'] = og_title.get("text", "")
         og_description = ask_gpt(prompts["translate"].format(text=metadata_properties['og:description']), json=True)
